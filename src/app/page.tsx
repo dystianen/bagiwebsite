86f046ca6c855fce49cef9c80d2b5eb736e36a53
Dystian En <distian.enyus@gmail.com>
2025-02-06 15:22:00 +0700

    fix: reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -376,30 +376,32 @@ const Home = () => {
       </Card>
 
       <section className="tw-h-[500px] tw-my-10 tw-flex tw-items-center tw-w-full">
-        <motion.div
-          initial={{ y: 100, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{
-            duration: 1
-          }}
-          viewport={{ once: true }}
-          style={{ width: '100%' }}
-        >
-          <Flex direction={'column'} align={'center'} mb={40} gap={16}>
-            <Text className="tw-text-2xl md:tw-text-4xl tw-text-center tw-font-semibold">
-              {t('reviews.reviews_header.title')}
-            </Text>
-            <Text className="tw-max-w-2xl tw-text-center tw-text-gray-500">
-              {t('reviews.reviews_header.description')}
-            </Text>
-          </Flex>
+        <Container size={'xl'}>
+          <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 1
+            }}
+            viewport={{ once: true }}
+            style={{ width: '100%' }}
+          >
+            <Flex direction={'column'} align={'center'} mb={40} gap={16}>
+              <Text className="tw-text-2xl md:tw-text-4xl tw-text-center tw-font-semibold">
+                {t('reviews.reviews_header.title')}
+              </Text>
+              <Text className="tw-max-w-2xl tw-text-center tw-text-gray-500">
+                {t('reviews.reviews_header.description')}
+              </Text>
+            </Flex>
 
-          <Marquee gradient={!isMobile} gradientColor={'var(--mantine-color-body)'}>
-            {review.map((it, index) => (
-              <ReviewCard key={index} name={it.name} rating={it.rating} comment={t(it.comment)} />
-            ))}
-          </Marquee>
-        </motion.div>
+            <Marquee gradient={!isMobile} gradientColor={'var(--mantine-color-body)'}>
+              {review.map((it, index) => (
+                <ReviewCard key={index} name={it.name} rating={it.rating} comment={t(it.comment)} />
+              ))}
+            </Marquee>
+          </motion.div>
+        </Container>
       </section>
     </>
   );
