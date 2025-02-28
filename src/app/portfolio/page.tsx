e0ba98bcfb9e48a6df0c08d899a8634fb2c50e31
Dystian En <distian.enyus@gmail.com>
2025-02-28 13:31:06 +0700

    fix: height screen

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -18,7 +18,7 @@ const Portfolio = () => {
 
   return (
     <>
-      <section className="tw-flex tw-flex-col md:tw-justify-center tw-h-screen">
+      <section className="tw-flex tw-flex-col md:tw-justify-center tw-h-screen md:tw-h-screen">
         {/* Wrapper untuk mobile dan desktop */}
         <div className="tw-container tw-mx-auto tw-text-center tw-mb-10 md:tw-mb-0 md:tw-absolute md:tw-inset-0 md:tw-flex md:tw-items-center md:tw-justify-center md:tw-z-10">
           <h1 className="tw-text-3xl md:tw-text-6xl lg:tw-text-8xl xl:tw-text-9xl tw-text-center tw-tracking-[0.4em]">
