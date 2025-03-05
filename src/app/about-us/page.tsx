05af1918c79163bb8403217d1f9bc85b1061181e
Dystian En <distian.enyus@gmail.com>
2025-03-05 15:56:49 +0700

    fix: metadat

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,12 +3,8 @@ import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { FC } from 'react';
 
-export async function generateMetadata({
-  params: { lang }
-}: {
-  params: { lang: string };
-}): Promise<Metadata> {
-  const t = await getTranslations({ locale: lang, namespace: 'Global' });
+export async function generateMetadata(locale: string): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'Global' });
   return { title: t('about_us') };
 }
 
